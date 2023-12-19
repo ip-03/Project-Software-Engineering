366740f2af1e5c1f272e6cdf725fd0d74fe53688
â <>
2023-12-19 11:11:10 +0100

    GUI

---
+++ projectsoftawre/GraphWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WeatherApp
+{
+    public partial class GraphWindow : Window
+    {
+        public GraphWindow()
+        {
+            InitializeComponent();
+        }
+        private void ChartLoading(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MoreDataButton(object sender, RoutedEventArgs e)
+        {
+
+
+            MoreDataWindow moreDataWindow = new MoreDataWindow(); 
+
+            
+            moreDataWindow.DataContext = this;
+
+            
+            moreDataWindow.Show();
+
+        }
+    }
+}
