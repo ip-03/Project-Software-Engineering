09c48eec0e9e4d2231df1ea1c7f35268403e85c8
â <>
2023-12-19 12:08:00 +0100

    add error handelings

--- projectsoftawre/MainWindow.xaml.cs
+++ projectsoftawre/MainWindow.xaml.cs
@@ -7,10 +7,12 @@ using System.Windows;
 using LiveCharts;
 using LiveCharts.Wpf;
 using System.Collections.Generic;
+using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace WeatherApp
 {
- 
+
     public partial class MainWindow : Window
     {
         private Main Main;
@@ -21,95 +23,107 @@ namespace WeatherApp
         public MainWindow() // constructor
         {
             InitializeComponent();
-            Main = new Main(); 
+            Main = new Main();
 
             // Initialize the graph properties
             ChartSeriesCollection = new SeriesCollection();
 
-           
+
         }
 
         private void DisplayButton(object sender, RoutedEventArgs e)
         {
             try
             {
-                var selectedDevices = new List<string>();
-
-                if (checkBoxSaxion.IsChecked == true)
-                    selectedDevices.Add("lht-saxion");
-                if (checkBoxGronau.IsChecked == true)
-                    selectedDevices.Add("lht-gronau");
-                if (checkBoxWierden.IsChecked == true)
-                    selectedDevices.Add("lht-wierden");
-
-                var selectedSensorType = (sensorTypeComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
-                var selectedDate = datePicker.SelectedDate ?? DateTime.Today;
-
-                var sensorData = Main.sensor_data
-                    .Where(sensor => selectedDevices.Contains(sensor.device_id) && sensor.date_time.Date == selectedDate)
-                    .OrderBy(sensor => sensor.date_time);
-
-                ChartSeriesCollection.Clear();
-
-                GraphWindow graphWindow = new GraphWindow();
-
-                // Set the DataContext for the GraphWindow
-                graphWindow.DataContext = this;
-
-                // Show the GraphWindow
-                graphWindow.Show();
-
-
-                foreach (var selectedDevice in selectedDevices)
+                if (checkBoxSaxion.IsChecked == false && checkBoxGronau.IsChecked == false && checkBoxWierden.IsChecked == false || datePicker.SelectedDate == null)
                 {
-                    LineSeries series = new LineSeries
-                    {
-                        Title = $"{selectedDevice} - {selectedSensorType}",
-                        Values = new ChartValues<double>()
-                    };
+                    MessageBox.Show("Please make sure that at least one city " +
+                        "or the date are selected! Select help for more information.",
+                        "Error");
+                }
+                else
+                {
+
+                    var selectedDevices = new List<string>();
+
+                    if (checkBoxSaxion.IsChecked == true)
+                        selectedDevices.Add("lht-saxion");
+                    if (checkBoxGronau.IsChecked == true)
+                        selectedDevices.Add("lht-gronau");
+                    if (checkBoxWierden.IsChecked == true)
+                        selectedDevices.Add("lht-wierden");
+
+                    var selectedSensorType = (sensorTypeComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+                    var selectedDate = datePicker.SelectedDate ?? DateTime.Today;
+
+                    var sensorData = Main.sensor_data
+                        .Where(sensor => selectedDevices.Contains(sensor.device_id) && sensor.date_time.Date == selectedDate)
+                        .OrderBy(sensor => sensor.date_time);
+
+                    ChartSeriesCollection.Clear();
+
+                    GraphWindow graphWindow = new GraphWindow();
+
+                    // Set the DataContext for the GraphWindow
+                    graphWindow.DataContext = this;
+
+                    // Show the GraphWindow
+                    graphWindow.Show();
 
 
-                    switch (selectedSensorType)
+                    foreach (var selectedDevice in selectedDevices)
                     {
-                        case "Temperature In":
-                            series.Values = new ChartValues<double>(sensorData
-                                .Where(sensor => sensor.device_id == "lht-saxion")
-                                .Select(sensor => sensor.temperature_in ?? 0));
-                            break;
-                        case "Temperature Out":
-                            series.Values = new ChartValues<double>(sensorData
-                                .Where(sensor => sensor.device_id == selectedDevice)
-                                .Select(sensor => sensor.temperature_out));
-                            break;
-                        case "Humidity":
-                            series.Values = new ChartValues<double>(sensorData
-                                .Where(sensor => sensor.device_id == selectedDevice)
-                                .Select(sensor => sensor.humidity));
-                            break;
-                        case "Ambient Light":
-                            series.Values = new ChartValues<double>(sensorData
-                                .Where(sensor => sensor.device_id == selectedDevice)
-                                .Select(sensor => sensor.ambient_light));
-                            break;
-                        case "Barometric Pressure":
-                            series.Values = new ChartValues<double>(sensorData
-                                .Where(sensor => sensor.device_id == selectedDevice)
-                                .Select(sensor => sensor.barometric_pressure ?? 0));
-                            break;
-                        default:
-                            break;
+                        LineSeries series = new LineSeries
+                        {
+                            Title = $"{selectedDevice} - {selectedSensorType}",
+                            Values = new ChartValues<double>()
+                        };
+
+
+                        switch (selectedSensorType)
+                        {
+                            case "Temperature In":
+                                series.Values = new ChartValues<double>(sensorData
+                                    .Where(sensor => sensor.device_id == "lht-saxion")
+                                    .Select(sensor => sensor.temperature_in ?? 0));
+                                break;
+                            case "Temperature Out":
+                                series.Values = new ChartValues<double>(sensorData
+                                    .Where(sensor => sensor.device_id == selectedDevice)
+                                    .Select(sensor => sensor.temperature_out));
+                                break;
+                            case "Humidity":
+                                series.Values = new ChartValues<double>(sensorData
+                                    .Where(sensor => sensor.device_id == selectedDevice)
+                                    .Select(sensor => sensor.humidity));
+                                break;
+                            case "Ambient Light":
+                                series.Values = new ChartValues<double>(sensorData
+                                    .Where(sensor => sensor.device_id == selectedDevice)
+                                    .Select(sensor => sensor.ambient_light));
+                                break;
+                            case "Barometric Pressure":
+                                series.Values = new ChartValues<double>(sensorData
+                                    .Where(sensor => sensor.device_id == selectedDevice)
+                                    .Select(sensor => sensor.barometric_pressure ?? 0));
+                                break;
+                            default:
+                                break;
+                        }
+
+                        series.LabelPoint = point => $"{selectedDevice}: {point.Y} \n Date: {selectedDate}";
+
+
+
+                        ChartSeriesCollection.Add(series);
+
+
                     }
-
-                    series.LabelPoint = point => $"{selectedDevice}: {point.Y} \n Date: {selectedDate}";
-
-                    
-
-                    ChartSeriesCollection.Add(series);
                 }
             }
             catch (Exception ex)
             {
-
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -118,7 +132,7 @@ namespace WeatherApp
         {
             DisplayButton(sender, e);
         }
-    
+
 
         private void ChartLoading(object sender, RoutedEventArgs e)
         {
@@ -126,13 +140,11 @@ namespace WeatherApp
         }
         private void HelpButton(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("No Help Currently", "Help", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show("Select the appropriate city to display the information of the set location. Information to be displayed as well can be changed accordingly. To proceed " +
+                "it is necessary to select at least one city and a date.", "Help");
         }
-      
-
     }
 
 
 }
-
 
